416105d0da4d422375eb3e19d93e9eec541b10ab
jdgrobeck <jdgrobeck@gmail.com>
2023-04-12 13:31:00 -0500

    Adding rest of files

---
+++ src/components/Beers.js
@@ -0,0 +1,69 @@
+
+import React, { Component } from "react";
+import Beer from "./Beer";
+import './Drinks.css'
+import Like from './Like'
+
+export default class Beers extends Component { 
+    constructor(props) {
+        console.log("constructor initialized")
+        super(props)
+
+        this.state = {
+            drinks: [],
+            likedBeer: true
+        }
+    }
+
+    componentDidMount() {
+        console.log("component mounted")
+        fetch("https://api.punkapi.com/v2/beers")
+        .then ( (response) => response.json() )
+        .then ( (data) =>
+            this.setState({
+                drinks: data
+            }))
+    }
+
+    componentDidUpdate() {
+        console.log("component updated", this.state.drinks)
+    }
+
+    handleClick = () => {
+        this.setState({
+          likedBeer: !this.state.likedBeer
+        });
+      };
+
+    render() {
+        console.log("render")
+       return (
+        <div>
+            <h2>Beers!</h2>
+            <div className="drink-info">
+                {this.state.drinks.map( (drink) => {
+                    return(
+                        <div>
+                        <Beer drink = {drink} />
+                        {this.state.likedBeer ? "" : <Like />}
+                        {this.state.likedBeer ? <button onClick={this.handleClick}>Like this beer</button> : <button onClick={this.handleClick}>Unike this beer</button>}
+                        </div>
+                        // <li>
+                        //     <img src={actor.image} alt={actor.name} />
+                        //     <h4>{actor.name}</h4>
+                        //     <p>{actor.location.name}</p>
+                        // </li>
+                    )
+                })
+                
+                 
+                }
+                
+                
+            </div> 
+
+
+        </div>
+       )
+    }
+}
